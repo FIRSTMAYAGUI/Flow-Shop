e8aad3016021535f15b52b18f7dfb728364e9229
FIRSTMAYAGUI <firstmayaguijunior@gmail.com>
2025-12-07 01:24:37 +0100

    Fix middleware not well applied

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,29 +21,29 @@ Route::controller(AuthController::class)->group(function (){
 
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{productId}', [ProductController::class, 'show']);
-Route::controller(ProductController::class)->group(function (){
+Route::middleware('auth:sanctum')->controller(ProductController::class)->group(function (){
     Route::post('/products', 'store');
     Route::put('/products/{productId}', 'update');
     Route::delete('/products/{productId}', 'destroy');
-})->middleware('auth:sanctum');
+});
 
 Route::get('/categories', [CategoriesController::class, 'index']);
-Route::controller(CategoriesController::class)->group(function (){
+Route::middleware('auth:sanctum')->controller(CategoriesController::class)->group(function (){
     Route::post('/categories', 'store');
     Route::get('/categories/{categoryId}', 'show');
     Route::put('/categories/{categoryId}', 'update');
     Route::delete('/categories/{categoryId}', 'destroy');
-})->middleware('auth:sanctum');
+});
 
-Route::controller(FavoritesController::class)->group(function (){
+Route::middleware('auth:sanctum')->controller(FavoritesController::class)->group(function (){
     Route::post('/favorites', 'store');
     Route::get('/favorites/{userId}', 'index');
     Route::get('/favorites/{favoriteId}/{userId}', 'show');
     Route::delete('/favorites/{favoriteId}/{userId}', 'destroy');
-})->middleware('auth:sanctum');
+});
 
-Route::controller(OrdersController::class)->group(function (){
+Route::middleware('auth:sanctum')->controller(OrdersController::class)->group(function (){
     Route::post('/users/orders', 'store');
     Route::get('/users/orders/{userId}', 'index');
     Route::get('/users/orders/{userId}/{orderId}', 'show');
-})->middleware('auth:sanctum');
+});
