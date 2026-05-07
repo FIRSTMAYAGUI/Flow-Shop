963c36aa47ed29bf940b0111881d7f21cf4e64da
FIRSTMAYAGUI <firstmayaguijunior@gmail.com>
2026-05-08 00:15:54 +0200

    feat(add): add image_url and icon_url to Category type

--- frontend/src/features/categories/categoryTypes.ts
+++ frontend/src/features/categories/categoryTypes.ts
@@ -2,6 +2,8 @@ export type Category = {
     id: number | string;
     name: string ;
     slug: string;
+    image_url: string;
+    icon_url: string;
     created_at: string;
     updated_at: string;
 }
